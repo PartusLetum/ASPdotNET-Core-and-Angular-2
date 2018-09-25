f817f2f40420cb9a7459b2cb1af26f9782f70b08
PartusLetum <partusletum@yahoo.com>
2018-09-25 22:59:57 +0100

    Chapter the third, part 1.

--- src/WebApplication2/Scripts/app/app.module.ts
+++ src/WebApplication2/Scripts/app/app.module.ts
@@ -1,15 +1,20 @@
-﻿///<reference path="../../typings/index.d.ts"/> 
+﻿
+///<reference path="../../typings/index.d.ts"/> 
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import { HttpModule } from "@angular/http";
 import "rxjs/Rx";
 
 import { AppComponent } from "./app.component";
+import { ItemListComponent } from "./item-list.component";
+
+import { ItemService } from "./item.service";
 
 @NgModule({
     // directives, components, and pipes 
     declarations: [
-        AppComponent
+        AppComponent,
+        ItemListComponent
     ],
     // modules 
     imports: [
@@ -18,6 +23,7 @@ import { AppComponent } from "./app.component";
     ],
     // providers 
     providers: [
+        ItemService
     ],
     bootstrap: [
         AppComponent
